6833b8578e60eadde37daa1aa087705f67ba8dc0
Damian Andrysiak <andrydamian@gmail.com>
2022-03-19 21:21:15 +0100

    added gradient descent

---
+++ 1_Year/Optimization_Methods/Gradient_Descent/Gradient_Descent/main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+
+
+float f(const float x, const float y);
+float df_x(const float x, const float y);
+float df_y(const float x, const float y);
+
+
+int main(int argc, char* argv[])
+{
+
+	float x{ 1.f };
+	float y{ 2.f };
+	float alpha{ 0.01f };
+	const float E{ 0.001f };
+
+	for (std::size_t i = 0; i < 10000; ++i)
+	{
+		float temp_x = x;
+		float temp_y = y;
+
+		temp_x = temp_x - alpha * df_x(temp_x, temp_y);
+		temp_y = temp_y - alpha * df_y(temp_x, temp_y);
+
+		x = temp_x;
+		y = temp_y;
+
+
+		const float L1_norm = std::fabs(df_x(temp_x, temp_y)) + std::fabs(df_y(temp_x, temp_y));
+		if (L1_norm <= E) //L1 Norm -> better sparsity
+		{
+			break;
+		}
+
+		//const float L2_norm = std::powf(std::fabs(df_x(temp_x, temp_y)), 2.f) + std::powf(std::fabs(df_y(temp_x, temp_y)), 2.f);
+		//if (std::sqrt(L2_norm )<= E) //L2 Norm 
+		//{
+		//	break;
+		//}
+
+	}
+
+	std::cout << "X: " << x << " | Y: " << y << '\n';
+
+
+	std::cin.get();
+	return EXIT_SUCCESS;
+}
+
+float f(const float x, const float y)
+{
+	return (-1.f * std::powf(x, 2.f) - 3.f * y) * std::exp(-1.f * std::powf(x, 2.f) - std::powf(y, 2.f));
+}
+
+float df_x(const float x, const float y)
+{
+	return 2.f * std::exp(-1.f * std::powf(x, 2.f) - std::powf(y, 2.f)) * x * (-1.f + std::powf(x, 2.f) + 3.f * y);
+}
+
+float df_y(const float x, const float y)
+{
+	return std::exp(-1.f * std::powf(x, 2.f) - std::powf(y, 2.f)) * (-3.f + 2.f * std::powf(x, 2.f) * y + 6.f * std::powf(y, 2.f));
+}
